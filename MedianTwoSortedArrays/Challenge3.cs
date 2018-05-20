b9b59f6802cf323d186409ce543693a85b5da959
Rene Hinojosa <rphs09@hotmail.com>
2018-05-19 22:39:05 -0500

    Added Challenge 3

---
+++ MedianTwoSortedArrays/Challenge3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedianTwoSortedArrays
+{
+	public class Challenge3
+	{
+
+		/// <summary>
+		/// There are two sorted arrays nums1 and nums2 of size m and n respectively.
+		/// Find the median of the two sorted arrays.
+		/// </summary>
+		/// <param name="nums1"></param>
+		/// <param name="nums2"></param>
+		/// <returns>Double</returns>
+		public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			try 
+			{
+				List<int> mergeArray = new List<int>(nums1);
+				int centerIndex = 0;
+				double result = 0;
+
+				mergeArray.AddRange(nums2);
+				mergeArray.Sort();
+
+				int totalFields = mergeArray.Count();
+				if (totalFields % 2 == 0)
+				{
+					centerIndex = totalFields / 2;
+					result = Convert.ToDouble((mergeArray[centerIndex - 1] + mergeArray[centerIndex])) / 2;
+				}
+				else
+				{
+					centerIndex = (totalFields + 1) / 2 - 1;
+					result = mergeArray[centerIndex];
+				}
+				return result;
+
+			}	
+			catch(Exception e) 
+			{	
+				//TODO add log error here if needed. 
+			    return 0;
+			}
+		}
+
+	}
+}
---
+++ MedianTwoSortedArraysTests/Challenge3Tests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedianTwoSortedArrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedianTwoSortedArrays.Tests
+{
+	[TestClass()]
+	public class Challenge3Tests
+	{
+		[TestMethod()]
+		public void MedianOf13and2()
+		{
+			int[] num1 = new int[] { 1, 3 };
+			int[] num2 = new int[] { 2 };
+
+			Challenge3 ch3 = new Challenge3();
+
+			Assert.AreEqual(2.0, ch3.FindMedianSortedArrays(num1, num2));
+		}
+
+		[TestMethod]
+		public void MedianOf12and34()
+		{
+			int[] num1 = new int[] { 1, 2 };
+			int[] num2 = new int[] { 3, 4 };
+
+			Challenge3 ch3 = new Challenge3();
+
+			Assert.AreEqual(2.5, ch3.FindMedianSortedArrays(num1, num2));
+		}
+
+		[TestMethod]
+		public void MedianOfEmptyand1()
+		{
+			int[] num1 = new int[] {  };
+			int[] num2 = new int[] { 1 };
+
+			Challenge3 ch3 = new Challenge3();
+
+			Assert.AreEqual(1.0, ch3.FindMedianSortedArrays(num1, num2));
+		}
+
+		[TestMethod]
+		public void MedianOfEmptyand1234()
+		{
+			int[] num1 = new int[] { };
+			int[] num2 = new int[] { 1,2,3,4 };
+
+			Challenge3 ch3 = new Challenge3();
+
+			Assert.AreEqual(2.5, ch3.FindMedianSortedArrays(num1, num2));
+		}
+
+		[TestMethod]
+		public void MedianOf11and12()
+		{
+			int[] num1 = new int[] { 1, 1 };
+			int[] num2 = new int[] { 1, 2 };
+
+			Challenge3 ch3 = new Challenge3();
+
+			Assert.AreEqual(1.0, ch3.FindMedianSortedArrays(num1, num2));
+		}
+	}
+}
