58c46a246252544e50639ee094aff61d5fd18ec6
Rene Hinojosa <rphs09@hotmail.com>
2018-05-19 19:04:00 -0500

    Solved challenge 2

---
+++ ReverseInteger/Challenge2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger
+{
+	public class Challenge2
+	{
+		/// <summary>
+		/// Given a 32-bit signed integer, reverse digits of an integer.
+		/// </summary>
+		/// <param name="number">The number to manipulate</param>
+		/// <returns>The new number</returns>
+		public int ReverseNumber(int number)
+		{
+			try 
+			{
+				int signNumber = Math.Sign(number);
+				int absNumber = Math.Abs(number);
+
+				var reversedNumber = absNumber.ToString().ToCharArray().Reverse();
+				var newNumber = Convert.ToInt32(String.Join("", reversedNumber));
+
+				return newNumber * signNumber;
+			}
+			catch(Exception e)
+			{
+				return 0;
+			}
+		}
+	}
+}
---
+++ ReverseIntegerTests/Challenge2Tests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseInteger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger.Tests
+{
+	[TestClass()]
+	public class Challenge2Tests
+	{
+		[TestMethod()]
+		public void ReverseNumberTest()
+		{
+			Challenge2 ch2 = new Challenge2();
+			Assert.AreEqual(-321, ch2.ReverseNumber(-123));
+		}
+
+		[TestMethod()]
+		public void Reverse2568()
+		{
+			Challenge2 ch2 = new Challenge2();
+			Assert.AreEqual(8652, ch2.ReverseNumber(2568));
+		}
+
+		[TestMethod()]
+		public void Reverse120()
+		{
+			Challenge2 ch2 = new Challenge2();
+			Assert.AreEqual(21, ch2.ReverseNumber(120));
+		}
+	}
+}
